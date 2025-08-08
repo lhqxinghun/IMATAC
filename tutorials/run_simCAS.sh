d0f255a79f327500e2d255f78c7c37b2bce3d284
Hongqiang Lyu <hongqianglv@mail.xjtu.edu.cn>
2025-08-08 15:05:02 +0800

    Create run_simCAS.sh

---
+++ tutorials/run_simCAS.sh
@@ -0,0 +1,1 @@
+python simCAS.py --simu_type discrete --n_cell_total 2500 --activation exp_linear --tree_text '(((A:0.2,B:0.2):0.2,C:0.4):0.5,(D:1,E:1):1);' --pops_name A B C D E --zero_prob 0.6 --n_peak 80000
